a52b609d41017e9816913b5f9a3ce6031de09077
Kelly Lam <klam@student.ysu.edu>
2022-09-02 19:19:37 -0400

    implemented home page images + footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'WELCOME';
+  desc = 'We offer guests a Teppanyaki dining experience along with a variety of sushi and sashimi options. We look forward to serving you!';
 
   constructor() { }
 
