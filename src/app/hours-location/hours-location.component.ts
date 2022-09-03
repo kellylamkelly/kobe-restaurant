1139065bfb29d022bb65472a1f33e6ee1e5b361f
Kelly Lam <klam@student.ysu.edu>
2022-09-02 22:28:53 -0400

    added Hours & Location title, address, and phone number

--- src/app/hours-location/hours-location.component.ts
+++ src/app/hours-location/hours-location.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hours-location.component.css']
 })
 export class HoursLocationComponent implements OnInit {
+  address = '2499 E State St,';
+  city = 'Hermitage, PA 16148';
+  phoneNum = '(724) 981-9955';
 
   constructor() { }
 
