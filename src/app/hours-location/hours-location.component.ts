8864d266260f7b13b2709126717a4809e10fbac8
Kelly Lam <2020lamk@gmail.com>
2022-09-02 23:47:52 -0400

    added hours and map to Hours & Location page

--- src/app/hours-location/hours-location.component.ts
+++ src/app/hours-location/hours-location.component.ts
@@ -9,6 +9,10 @@ export class HoursLocationComponent implements OnInit {
   address = '2499 E State St,';
   city = 'Hermitage, PA 16148';
   phoneNum = '(724) 981-9955';
+  monHours = 'Closed';
+  tuesThursHours = '4 — 8:30PM';
+  friSatHours = '12 — 9PM';
+  sunHours = '3 — 8PM';
 
   constructor() { }
 
