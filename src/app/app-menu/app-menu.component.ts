40c90c0196cf096a3f33229749676bcbb5b61271
Kelly Lam <2020lamk@gmail.com>
2022-09-27 17:06:39 -0400

    created json file for menus and began implementing component layout for appetizers

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import menuJson from 'src/assets/menu.json';
 
 @Component({
   selector: 'app-app-menu',
   templateUrl: './app-menu.component.html',
   styleUrls: ['./app-menu.component.css']
 })
+
 export class AppMenuComponent implements OnInit {
+
+  soupAndSalads = menuJson.menu.appetizers.soupAndSalads;
+  apps = menuJson.menu.appetizers.apps;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
