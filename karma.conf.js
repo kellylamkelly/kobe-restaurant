3d858b05a71267db36deb1d7df045c08859e6b8c
Kelly Lam <klam@student.ysu.edu>
2022-09-28 11:19:59 -0400

    renamed repo from 4890-final-project to kobe-restaurant

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/4890-final-project'),
+      dir: require('path').join(__dirname, './coverage/kobe-restaurant'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '4890-Final-Project'`, () => {
+  it(`should have as title 'Kobe-Restaurant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('4890-Final-Project');
+    expect(app.title).toEqual('Kobe-Restaurant');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('4890-Final-Project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Kobe-Restaurant app is running!');
   });
 });
