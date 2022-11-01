a63975f408ba20989bbc8ee097a83dff19aca546
Kelly Lam <klam@student.ysu.edu>
2022-11-01 16:28:14 -0400

    added beverages portion of the menu

--- src/app/menu/beverages/beverages.component.ts
+++ src/app/menu/beverages/beverages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import menuJson from 'src/assets/menu.json';
 
 @Component({
   selector: 'app-beverages',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beverages.component.css']
 })
 export class BeveragesComponent implements OnInit {
+
+  menus = [
+    {label: "DRINKS", field: menuJson.menu.beverages.nonAlcohol},
+    {label: "COCKTAILS", field: menuJson.menu.beverages.cocktails}, 
+    {label: "BEERS", field: menuJson.menu.beverages.beers},
+    {label: "WINES", field: menuJson.menu.beverages.wine}
+  ]
 
   constructor() { }
 
