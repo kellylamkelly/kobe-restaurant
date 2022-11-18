9de8a8c440015e7539f73ad6df7f98d6f8f8a889
Kelly Lam <klam@student.ysu.edu>
2022-11-18 15:57:09 -0500

    created online order menu template and adjusted menu layout

--- src/app/order-online/order-online.component.ts
+++ src/app/order-online/order-online.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import menuJson from 'src/assets/menu.json';
 
 @Component({
   selector: 'app-order-online',
@@ -7,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderOnlineComponent implements OnInit {
 
+  appMenus = [
+    {label: "SOUP & SALADS", field: menuJson.menu.appetizers.soupAndSalads},
+    {label: "APPETIZERS", field: menuJson.menu.appetizers.apps}, 
+    {label: "SUSHI APPETIZERS", field: menuJson.menu.appetizers.sushiApps}, 
+    {label: "NOODLES & RICE", field: menuJson.menu.appetizers.noodlesAndRice}
+  ]
+
+  teppanyakiMenus = [
+    {label: "SINGLE CHOICE", field: menuJson.menu.teppanyaki.singles},
+    {label: "SENIOR & CHILDREN", field: menuJson.menu.teppanyaki.seniorAndChildren}, 
+    {label: "SUMO CHOICE", field: menuJson.menu.teppanyaki.sumos}, 
+    {label: "DOUBLE CHOICE", field: menuJson.menu.teppanyaki.doubles},
+    {label: "TRIPLE CHOICE", field: menuJson.menu.teppanyaki.triples},
+    {label: "EXTRAS", field: menuJson.menu.teppanyaki.extras}
+  ]
+
+  sushiMenus = [
+    {label: "NIGIRI", field: menuJson.menu.sushi.nigiri},
+    {label: "ENTR\u00C9ES", field: menuJson.menu.sushi.entrees}, 
+    {label: "SUSHI ROLLS", field: menuJson.menu.sushi.rolls}
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
