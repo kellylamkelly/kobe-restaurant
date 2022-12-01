cf16825a817528b9b214dcdcbaaec1dd600eb866
Kelly Lam <2020lamk@gmail.com>
2022-12-01 01:40:50 -0500

    added teppanyaki meals message, added modal but not implemented

--- src/app/order-online/order-online.component.ts
+++ src/app/order-online/order-online.component.ts
@@ -9,30 +9,56 @@ import menuJson from 'src/assets/menu.json';
 
 export class OrderOnlineComponent implements OnInit {
   appMenus = [
-    {label: "SOUP & SALADS", field: menuJson.menu.appetizers.soupAndSalads},
-    {label: "APPETIZERS", field: menuJson.menu.appetizers.apps}, 
-    {label: "SUSHI APPETIZERS", field: menuJson.menu.appetizers.sushiApps}, 
-    {label: "NOODLES & RICE", field: menuJson.menu.appetizers.noodlesAndRice}
+    { label: "SOUP & SALADS", field: menuJson.menu.appetizers.soupAndSalads },
+    { label: "APPETIZERS", field: menuJson.menu.appetizers.apps },
+    { label: "SUSHI APPETIZERS", field: menuJson.menu.appetizers.sushiApps },
+    { label: "NOODLES & RICE", field: menuJson.menu.appetizers.noodlesAndRice }
   ]
 
   teppanyakiMenus = [
-    {label: "SINGLE CHOICE", field: menuJson.menu.teppanyaki.singles},
-    {label: "STEAKS", field: menuJson.menu.teppanyaki.steaks},
-    {label: "SENIOR & CHILDREN", field: menuJson.menu.teppanyaki.seniorAndChildren}, 
-    {label: "SUMO CHOICE", field: menuJson.menu.teppanyaki.sumos}, 
-    {label: "DOUBLE CHOICE", field: menuJson.menu.teppanyaki.doubles},
-    {label: "TRIPLE CHOICE", field: menuJson.menu.teppanyaki.triples},
-    {label: "EXTRAS", field: menuJson.menu.teppanyaki.extras}
+    { label: "SINGLE CHOICE", field: menuJson.menu.teppanyaki.singles },
+    { label: "STEAKS", field: menuJson.menu.teppanyaki.steaks },
+    { label: "SENIOR & CHILDREN", field: menuJson.menu.teppanyaki.seniorAndChildren },
+    { label: "SUMO CHOICE", field: menuJson.menu.teppanyaki.sumos },
+    { label: "DOUBLE CHOICE", field: menuJson.menu.teppanyaki.doubles },
+    { label: "TRIPLE CHOICE", field: menuJson.menu.teppanyaki.triples },
+    { label: "EXTRAS", field: menuJson.menu.teppanyaki.extras }
   ]
 
   sushiMenus = [
-    {label: "NIGIRI", field: menuJson.menu.sushi.nigiri},
-    {label: "ENTR\u00C9ES", field: menuJson.menu.sushi.entrees}, 
-    {label: "SUSHI ROLLS", field: menuJson.menu.sushi.rolls}
+    { label: "NIGIRI", field: menuJson.menu.sushi.nigiri },
+    { label: "ENTR\u00C9ES", field: menuJson.menu.sushi.entrees },
+    { label: "SUSHI ROLLS", field: menuJson.menu.sushi.rolls }
   ]
-  
+
   constructor() { }
 
   ngOnInit(): void {
+
+    // // Get the modal
+    // var modal = document.getElementById("appModal") as HTMLDivElement || null;
+
+    // // Get the button that opens the modal
+    // var btn = document.getElementById("appModalButton") as HTMLButtonElement || null;
+
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0] as HTMLSpanElement || null;
+
+    // // When the user clicks the button, open the modal 
+    // btn.onclick = function () {
+    //   modal.style.display = "block";
+    // }
+
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function () {
+    //   modal.style.display = "none";
+    // }
+
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function (event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // }
   }
 }
